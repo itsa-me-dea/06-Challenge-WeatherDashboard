c611568006d9a2094c0417f23de88e7407f257b8
itsa-me-dea <wachadea@gmail.com>
2023-05-11 23:18:35 -0700

    Added JS to collect users search and store into local storage, updated README. NOTE: couldn't complete server side API functionality due to time constraints.

--- Assets/script.js
+++ Assets/script.js
@@ -4,3 +4,142 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, options);
   });
+
+  // Coordinates by location name
+  // var coordindateAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=1da369cd245d58589a61e674c17f6b77';
+
+  // Main Weather API
+  // var apiUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat=' + lattitude + '&lon=' + longitude + '&exclude=alerts&units=imperial&appid=1da369cd245d58589a61e674c17f6b77';
+  
+  // collecting the user input from the search
+  // https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
+  var search = document.getElementById('search');
+  console.log(search);
+
+  search.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      var city = search.value;
+      // save user input into local storage
+      localStorage.setItem("Search History", city);
+      // Add city history to search history
+      // var history = document.getElementById('history');
+      
+      // history.innerHTML = '<option value="' + city + '">' + city + '</option>';
+      
+    }
+  });
+
+
+// adapted from 22-Stu_Review-Part-One
+// var userFormEl = document.querySelector('#user-form');
+// var languageButtonsEl = document.querySelector('#language-buttons');
+// var nameInputEl = document.querySelector('#username');
+// var repoContainerEl = document.querySelector('#repos-container');
+// var repoSearchTerm = document.querySelector('#repo-search-term');
+
+// var formSubmitHandler = function (event) {
+//   event.preventDefault();
+
+//   var username = nameInputEl.value.trim();
+
+//   if (username) {
+//     getRequestedWeather(username);
+
+//     repoContainerEl.textContent = '';
+//     nameInputEl.value = '';
+//   } else {
+//     alert('Please enter a GitHub username');
+//   }
+// };
+
+// var buttonClickHandler = function (event) {
+//   // `event.target` is a reference to the DOM element of what programming language button was clicked on the page
+//   var language = event.target.getAttribute('data-language');
+
+//   // If there is no language read from the button, don't attempt to fetch repos
+//   if (language) {
+//     getFeaturedRepos(language);
+
+//     repoContainerEl.textContent = '';
+//   }
+// };
+
+// var getRequestedWeather = function (user) {
+//   var apiUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat=' + lattitude + '&lon=' + longitude + '&exclude=alerts&units=imperial&appid=1da369cd245d58589a61e674c17f6b77';
+  
+
+//   fetch(apiUrl)
+//     .then(function (response) {
+//       if (response.ok) {
+//         console.log(response);
+//         response.json().then(function (data) {
+//           console.log(data);
+//           displayRepos(data, user);
+//         });
+//       } else {
+//         alert('Error: ' + response.statusText);
+//       }
+//     })
+//     .catch(function (error) {
+//       alert('Unable to connect to GitHub');
+//     });
+// };
+
+// var getFeaturedRepos = function (language) {
+//   // The `q` parameter is what language we want to query, the `+is:featured` flag adds a filter to return only featured repositories
+//   // The `sort` parameter will instruct GitHub to respond with all of the repositories in order by the number of issues needing help
+//   var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
+
+//   fetch(apiUrl).then(function (response) {
+//     if (response.ok) {
+//       response.json().then(function (data) {
+//         displayRepos(data.items, language);
+//       });
+//     } else {
+//       alert('Error: ' + response.statusText);
+//     }
+//   });
+// };
+
+// var displayRepos = function (repos, searchTerm) {
+//   if (repos.length === 0) {
+//     repoContainerEl.textContent = 'No repositories found.';
+//     // Without a `return` statement, the rest of this function will continue to run and perhaps throw an error if `repos` is empty
+//     return;
+//   }
+
+//   repoSearchTerm.textContent = searchTerm;
+
+//   for (var i = 0; i < repos.length; i++) {
+//     // The result will be `<github-username>/<github-repository-name>`
+//     var repoName = repos[i].owner.login + '/' + repos[i].name;
+
+//     var repoEl = document.createElement('div');
+//     repoEl.classList = 'list-item flex-row justify-space-between align-center';
+
+//     var titleEl = document.createElement('span');
+//     titleEl.textContent = repoName;
+
+//     repoEl.appendChild(titleEl);
+
+//     var statusEl = document.createElement('span');
+//     statusEl.classList = 'flex-row align-center';
+
+//     if (repos[i].open_issues_count > 0) {
+//       statusEl.innerHTML =
+//         "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+//     } else {
+//       statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+//     }
+
+//     repoEl.appendChild(statusEl);
+
+//     repoContainerEl.appendChild(repoEl);
+//   }
+// };
+
+// userFormEl.addEventListener('submit', formSubmitHandler);
+// languageButtonsEl.addEventListener('click', buttonClickHandler);
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
+**NOTE: Due to time constraints, this project is incomplete.**
 # 06-Challenge-WeatherDashboard
-Module 6 Challenge | GitLab Repo/06-Server-Side-APIs/02-Challenge
+
+### **[Description](#description) | [Installation](#installation) | [Usage](#usage) | [Credits](#credits) | [License](#license)**
+
+## Description
+
+The goal was to utilize server-side APIs, in this case [OpenWeather API](https://openweathermap.org/), to create a functional weather dashboard that can provide daily and five-day forcast of a requested city. This will provide the weather for the user to utilize for future planning.
+
+What did I learn? I definitely learned how to use how to better utilize third-party APIs. 
+
+## Installation
+
+No installation is required. See the following link to access the website: https://itsa-me-dea.github.io/06-Challenge-WeatherDashboard/
+
+## Usage
+
+Search for you desired city in the search bar. This will make a fetch request to the OpenWeather API and call the 5-day weather forcast for your inputted city.
+
+Your search history will be saved under the search history select option, where you can select one of these options to open the 5-day forcast again.
+
+<!-- ![Gif of my Weather Dashboard website](Assets/screen-capture.gif) -->
+
+## Credits
+
+* Materialize: https://materializecss.com/
+* OpenWeather: https://openweathermap.org/
+* A variety of topics and code were utilized and adapted from the following:
+    * UCSD-VIRT-FSF-PT-03-2023-U-LOLC/03-JavaScript/01-Activities
+    * UCSD-VIRT-FSF-PT-03-2023-U-LOLC/04-Web-APIs/01-Activities
+    * UCSD-VIRT-FSF-PT-03-2023-U-LOLC/05-Third-Party-APIs/01-Activities
+    * UCSD-VIRT-FSF-PT-03-2023-U-LOLC/06-Server-Side-APIs/01-Activities
+* **Note:** If I missed a link, it should be listed in the code comments with the adapted code following below said comment.
+
+## License
+
+Please refer to the [LICENSE](/LICENSE) within the repository.
